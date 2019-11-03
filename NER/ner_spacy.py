6cb129b7fc82a9fcda742ec1e335ee5a92774b6d
Jacob <kapsjacob@gmail.com>
2019-11-03 11:19:53 +0530

    defining main entry point for program

--- NER/ner_spacy.py
+++ NER/ner_spacy.py
@@ -7,10 +7,11 @@ import en_core_web_sm
 
 nlp = en_core_web_sm.load()
 
-doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
+if __name__ == "__main__":
+    doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
 
-#entity level
-pprint([(X.text, X.label_) for X in doc.ents])
+    #entity level
+    pprint([(X.text, X.label_) for X in doc.ents])
 
-#token level
-pprint([(X, X.ent_iob_, X.ent_type_) for X in doc])
+    #token level
+    pprint([(X, X.ent_iob_, X.ent_type_) for X in doc])
