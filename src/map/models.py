ad828cd597f62247ebef181a8a97f43cfbdcf546
Jacob <kapsjacob@gmail.com>
2019-11-04 10:56:01 +0530

    created multi class crime text classifier

--- src/map/models.py
+++ src/map/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class News():
-    title = 
-    crime = 
-    severity = 
-    state = 
-    city = 
+    title = models.CharField(max_length=60)
+    category = models.CharField(max_length=55)
+    severity = models.IntegerField(default=1,validators=[
+        MaxValueValidator(5),
+        MinValueValidator(1)
+    ])
+    state = models.CharField(max_length=35)
+    city = models.CharField(max_length=40)
