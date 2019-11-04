89aa0fdea3f7b2c01ebc11e5279d06d1e5e799c6
Jacob <kapsjacob@gmail.com>
2019-11-04 16:53:50 +0530

    added context to html page

--- src/map/views.py
+++ src/map/views.py
@@ -6,4 +6,9 @@ def map_view(request, *args, **kwargs):
     print(args,kwargs)
     print(request.user)
     #return HttpResponse("<h1>Hello World</h1>")
-    return render(request, "india.html", {})
+
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123
+    }
+    return render(request, "india.html", my_context)
