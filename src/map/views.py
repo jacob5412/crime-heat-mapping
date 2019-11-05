237d21f9c61033d63c7b526e9dcf4e0df45fa425
Jacob <kapsjacob@gmail.com>
2019-11-06 01:06:58 +0530

    completed map implementation

--- src/map/views.py
+++ src/map/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Sum
 from .models import News
+import itertools
 
 # Create your views here.
 def map_view(request, *args, **kwargs):
@@ -9,8 +11,26 @@ def map_view(request, *args, **kwargs):
     
     #return HttpResponse("<h1>Hello World</h1>") # string of html
 
-    crime = News.objects.all()
+    crime = []
+
+    states = News.objects.values('statecode')
+    statelist = set([state['statecode'] for state in states])
+
+    print(statelist)
+    for statecode in statelist:
+        aggregate = {}
+        aggregate['statecode'] = statecode
+        total = News.objects.filter(statecode=statecode).aggregate(Sum('severity'))
+        aggregate['severity'] = total['severity__sum']
+
+        state = News.objects.filter(
+            statecode=statecode).values('state')[0]['state']
+        
+        aggregate['state'] = state
+        crime.append(aggregate)
+
+    print(crime)
     crime_data = {
-        "crime_id": crime
+        "crimes": crime,
     }
     return render(request, "india.html", crime_data)
