ab5446b5bb2f9e456ead4f80ab71f686e37376e7
Jacob <kapsjacob@gmail.com>
2019-11-06 01:08:37 +0530

    added comments

--- src/map/views.py
+++ src/map/views.py
@@ -12,21 +12,29 @@ def map_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello World</h1>") # string of html
 
     crime = []
-
     states = News.objects.values('statecode')
+
+    # getting a set of unique statecodes
     statelist = set([state['statecode'] for state in states])
 
     print(statelist)
+
+    # iterating through state codes
     for statecode in statelist:
         aggregate = {}
         aggregate['statecode'] = statecode
+
+        # aggregating total severity for each statecode
         total = News.objects.filter(statecode=statecode).aggregate(Sum('severity'))
         aggregate['severity'] = total['severity__sum']
 
+        # retreiving state name for statecode
         state = News.objects.filter(
             statecode=statecode).values('state')[0]['state']
         
         aggregate['state'] = state
+
+        # appending result to crime list
         crime.append(aggregate)
 
     print(crime)
